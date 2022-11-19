c8caa1f32d0aaa82f59a7020c62e0b65da89d23f
Rob Hammet <hammetr@hotmail.com>
2022-11-18 20:32:15 -0500

    beautifying post component

--- src/app/component/post/post.component.ts
+++ src/app/component/post/post.component.ts
@@ -11,9 +11,13 @@ export class PostComponent implements OnInit {
   @Input() post : Post;
   @ViewChild('post_image')imageElementId!: ElementRef;
 
+  postAge : any;
+
+
   constructor() {
     this.post = {
     postText: "",
+    username:"",
     postId : 0,
     userId : 0,
     liked : 0,
@@ -34,7 +38,10 @@ export class PostComponent implements OnInit {
   }
 
    ngAfterViewInit() {
-    
+      this.postAge = Date.now() - this.post.date;
+
+
+
       let base64String : string = this.arrayBufferToBase64(this.post.postPhoto);
       let imageElement = this.imageElementId.nativeElement as HTMLImageElement;
     //  imageElement.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Crocodile_de_Morelet.jpeg/1200px-Crocodile_de_Morelet.jpeg";
